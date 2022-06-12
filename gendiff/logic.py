82560f508b0eb86c16e81209b359ebba6865bd94
Никита Дементьев <ambrosko@MacBook-Air-Nikita.local>
2022-06-12 13:35:47 +0300

    fixed_bugs

--- gendiff/logic.py
+++ gendiff/logic.py
@@ -22,9 +22,10 @@ def generate_diff(first_file, second_file):
             prefix = '-'
             if data_second_file.get(key) == data_first_file.get(key):
                 prefix = ' '
-            answer += f'{prefix} {key}: {true_style(data_first_file[key])}\n'
+            answer += f'  {prefix} {key}: {true_style(data_first_file[key])}\n'
         if key in data_second_file and prefix != ' ':
             prefix = '+'
-            answer += f'{prefix} {key}: {true_style(data_second_file[key])}\n'
+            answer += f'  {prefix} {key}: ' \
+                      f'{true_style(data_second_file[key])}\n'
     answer += '}'
     return answer
