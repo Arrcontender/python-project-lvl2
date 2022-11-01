2f3f4c68fcf158e8b1d854c9517059d4d7dbdc3c
Arrcontender <dramman4@gmail.com>
2022-11-01 23:43:02 +0300

    6_step

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,37 +1,101 @@
-from gendiff.file_parser import json_parse, yaml_parse
+import json
+import yaml
+import itertools
 
 
-def generate_diff(file1, file2):
-    if '.json' in file1 and '.json' in file2:
-        parsed_file1, parsed_file2 = json_parse(file1), json_parse(file2)
-    else:
-        parsed_file1, parsed_file2 = yaml_parse(file1), yaml_parse(file2)
+def stylish(value: dict, replacer=' ', spaces_count=2):
+    def iter_(current_value, depth):
+        if not isinstance(current_value, dict):
+            return str(current_value)
+        if depth == 0:
+            deep_indent_size = depth + spaces_count
+            current_indent = replacer * depth
+        else:
+            deep_indent_size = depth + spaces_count + 2
+            current_indent = replacer * (depth + 2)
+        deep_indent = replacer * deep_indent_size
+        lines = []
+        for key, val in current_value.items():
+            lines.append(f'{deep_indent}{key}: {iter_(val, deep_indent_size)}')
 
-    data1, data2 = parsed_file1, parsed_file2
+        result = itertools.chain("{", lines, [current_indent + "}"])
+        return '\n'.join(result)
 
-    diff1, diff2 = list(), list()
+    return iter_(value, 0)
 
-    for k, v in data1.items():
-        if k in data2:
-            if data1[k] == data2[k]:
-                diff1.append(f'    {k}: {data1[k]}')
+
+def tree_bool(value):
+    if value is False:
+        return 'false'
+    elif value is True:
+        return 'true'
+    elif value is None:
+        return 'null'
+    return value
+
+
+def compare(sign_read: dict, compare_read: dict, sign: str):
+    view_difference = dict()
+    for key in sign_read:
+        if not isinstance(sign_read[key], dict):
+            if key in compare_read and sign_read[key] == compare_read[key]:
+                view_difference['  ' + str(key)] = tree_bool(sign_read[key])
             else:
-                diff1.append(f'  - {k}: {data1[k]}')
+                view_difference[sign + ' ' + str(key)] = \
+                    tree_bool(sign_read[key])
         else:
-            diff1.append(f'  - {k}: {data1[k]}')
+            if key not in compare_read or not \
+                    isinstance(compare_read[key], dict):
+                compare_sign_value = compare(sign_read[key],
+                                             sign_read[key], ' ')
+                view_difference[sign + ' ' + str(key)] = compare_sign_value
+            elif key in compare_read and isinstance(compare_read[key], dict):
+                compare_sign_value = compare(sign_read[key],
+                                             compare_read[key], sign)
+                view_difference['  ' + str(key)] = compare_sign_value
+    return view_difference
 
-    for k, v in data2.items():
-        if k in data1:
-            if data2[k] != data1[k]:
-                diff2.append(f'  + {k}: {data2[k]}')
-        else:
-            diff2.append(f'  + {k}: {data2[k]}')
 
-    diff = diff1 + diff2
-    diff.sort(key=lambda i: i[4])
-    for i, v in enumerate(diff):
-        diff[i] = ''.join(map(lambda x: str(x).lower(), v))
-    diff.insert(0, '{')
-    diff.append('}')
-    res = '\n'.join(diff)
-    return res
+def is_json(file):
+    if file[-5:] == '.json':
+        return True
+    return False
+
+
+def parse_file(file):
+    if is_json(file):
+        return json.load(open(file))
+    else:
+        parse_file = yaml.safe_load(open(file))
+        return parse_file if parse_file is not None else {}
+
+
+def recursive_update(first_dict, second_dict):
+    for key in second_dict:
+        if key not in first_dict:
+            first_dict[key] = second_dict[key]
+        elif key in first_dict:
+            if isinstance(first_dict[key], dict) and \
+                    isinstance(second_dict[key], dict):
+                recursive_update(first_dict[key], second_dict[key])
+                first_dict[key] = sort_dict(first_dict[key])
+            else:
+                first_dict[key] = second_dict[key]
+
+
+def sort_dict(dictionary):
+    sorted_tuple = sorted(dictionary.items(), key=lambda x: x[0][2:])
+    return dict(sorted_tuple)
+
+
+def generate_diff(first_file, second_file, format=stylish):
+    first_read = parse_file(first_file)
+    second_read = parse_file(second_file)
+    first_diff = compare(first_read, second_read, '-')
+    second_diff = compare(second_read, first_read, '+')
+    recursive_update(first_diff, second_diff)
+    second_diff.clear()
+    sorted_tuple = sorted(first_diff.items(), key=lambda x: x[0][2:])
+    first_diff.clear()
+    difference = dict(sorted_tuple)
+    return format(difference)
--- gendiff/tests/test_generate_diff.py
+++ gendiff/tests/test_generate_diff.py
@@ -1,6 +1,4 @@
 from gendiff.generate_diff import generate_diff
-from gendiff.tests.fixtures.true_json import json_true
-from gendiff.tests.fixtures.true_yaml import yaml_true
 
 
 def test_generate_diff_type_json():
@@ -12,7 +10,8 @@ def test_generate_diff_type_json():
 def test_generate_diff_json():
     y = generate_diff('gendiff/tests/fixtures/file1.json',
                       'gendiff/tests/fixtures/file2.json')
-    assert json_true == y, 'Результат сравнения неверен'
+    with open('gendiff/tests/fixtures/true_json.txt', 'r') as expected:
+        assert expected.read() == y, 'Результат сравнения неверен'
 
 
 def test_generate_diff_type_yaml():
@@ -24,4 +23,21 @@ def test_generate_diff_type_yaml():
 def test_generate_diff_yaml():
     y = generate_diff('gendiff/tests/fixtures/file1.yml',
                       'gendiff/tests/fixtures/file2.yml')
-    assert yaml_true == y, 'Результат сравнения неверен'
+    with open('gendiff/tests/fixtures/true_yaml.txt', 'r') as expected:
+        assert expected.read() == y, 'Результат сравнения неверен'
+
+
+def test_nested_json():
+    first_file = 'gendiff/tests/fixtures/nested/nested_file1.json'
+    second_file = 'gendiff/tests/fixtures/nested/nested_file2.json'
+    with open('gendiff/tests/fixtures/nested/true_nested.txt', 'r') \
+            as expected:
+        assert generate_diff(first_file, second_file) == expected.read()
+
+
+def test_nested_yaml():
+    first_file = 'gendiff/tests/fixtures/nested/nested_file1.yml'
+    second_file = 'gendiff/tests/fixtures/nested/nested_file2.yml'
+    with open('gendiff/tests/fixtures/nested/true_nested.txt', 'r') \
+            as expected:
+        assert generate_diff(first_file, second_file) == expected.read()
